4f60cff0c38b076719bca3823dfd5a55c648a1f7
Egor Kostan <igorkostan@gmail.com>
2024-04-18 18:48:49 -0700

    Update main.py
    lesson 4

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
-from machine import Pin
-import time
+# Raspberry Pi Pico W LESSON 5: Reading Analog Voltages Using a Potentiometer
+# https://www.youtube.com/watch?v=ODWwErH_iGA&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyAhC5&index=5
 
-pin1 = Pin(13, Pin.OUT)
-pin2 = Pin(12, Pin.OUT)
-pin4 = Pin(11, Pin.OUT)
-pin8 = Pin(10, Pin.OUT)
-pins = [pin8, pin4, pin2, pin1]
-# binary = [8, 4, 2, 1]  # binary values
-
-numbers = {
-    0: [False, False, False, False],
-    1: [False, False, False, True],
-    2: [False, False, True, False],
-    3: [False, False, True, True],
-    4: [False, True, False, False],
-    5: [False, True, False, True],
-    6: [False, True, True, False],
-    7: [False, True, True, True],
-    8: [True, False, False, False],
-    9: [True, False, False, True],
-    10: [True, False, True, False],
-    11: [True, False, True, True],
-    12: [True, True, False, False],
-    13: [True, True, False, True],
-    14: [True, True, True, False],
-    15: [True, True, True, True],
-}
+"""
+In this introductory video, I will show how to read analog voltages using the Raspberry Pi PIco W.
+Fortunately, this is much easier than on the Raspberry PI, sine the Pico has a built in analog to
+digital converter. I will show how to read voltages using a potentiometer. This class is  for absolute
+beginners, and I do not assume you already understand the material I am presenting. My goal is not to 'Show Off',
+but to genuinely teach you how you can do this type of work and projects on your own. I will show all work
+step-by-step, with clear instructions.
+Enjoy!
+"""
 
 
-def get_val(n):
-    return 1 if n is True else 0
+import machine
+from time import sleep
 
-
-def set_pin_val(pin, val):
-    pin.value(val)
-
-
-def set_all_pins(all_pins, vals):
-    for i, v in enumerate(vals):
-        val = get_val(v)
-        pin = all_pins[i]
-        set_pin_val(pin, val)
+pin_read = machine.ADC(28)
+bit_to_volt_ratio = 18724.28
 
 
 if __name__ == '__main__':
 
     while True:
-        for i in range(0, 16):
-            n = numbers[i]
-            set_all_pins(pins, n)
-            print(f"number: {i}, pins: {n}")
-            time.sleep(3)
+        v = pin_read.read_u16()
+        print(f'voltage: {round(v / bit_to_volt_ratio, 2)}')
+        sleep(0.3)
