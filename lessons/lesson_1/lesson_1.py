5d04fab0dc5360a3a5d659915cd45eb2aff02e64
Egor Kostan <igorkostan@gmail.com>
2024-05-07 21:16:19 -0700

    Pylint fixes
    ************* Module lesson_1
    lessons/lesson_1/lesson_1.py:11:0: C0301: Line too long (115/100) (line-too-long)
    lessons/lesson_1/lesson_1.py:15:0: E0401: Unable to import 'machine' (import-error)
    lessons/lesson_1/lesson_1.py:22:0: C0103: Constant name "val" doesn't conform to UPPER_CASE naming style (invalid-name)
    lessons/lesson_1/lesson_1.py:30:8: C0103: Constant name "val" doesn't conform to UPPER_CASE naming style (invalid-name)
    lessons/lesson_1/lesson_1.py:16:0: C0411: standard import "time" should be placed before third party import "machine.Pin" (wrong-import-order)
    lessons/lesson_1/lesson_1.py:17:0: C0411: standard import "random" should be placed before third party import "machine.Pin" (wrong-import-order)

--- lessons/lesson_1/lesson_1.py
+++ lessons/lesson_1/lesson_1.py
@@ -8,24 +8,26 @@ I will show you how to install Thonny, the IDE, on your PC.
 Thonny will allow you to interact with the Pico W. Then in today's short introductory video,
 you will write your first four programs, and will get a homework assignment. This class is  for
 absolute beginners, and I do not assume you already understand the material I am presenting.
-My goal is not to 'Show Off', but to genuinely teach you how you can do this type of work and projects on your own.
+My goal is not to 'Show Off', but to genuinely teach you how you can do this type of work
+and projects on your own.
 Enjoy!
 """
 
-from machine import Pin
 import time
 import random
+from machine import Pin
+
 
 random.randrange(0,1)
 
 led = Pin("LED", Pin.OUT)
-val = 0
+pin_value = 0
 
 if __name__ == '__main__':
     while True:
         t = random.random()
         t = round(t, 2)
-        led.value(val)
+        led.value(pin_value)
         time.sleep(t)
-        val = 1 if val == 0 else 0
-        print(f"sleep: {t}, LED:{'ON' if val == 1 else 'OFF'}")
+        pin_value = 1 if pin_value == 0 else 0
+        print(f"sleep: {t}, LED:{'ON' if pin_value == 1 else 'OFF'}")
