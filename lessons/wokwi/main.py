e2e3ad2a28920b636aa8cf643104228c62474ca1
Egor Kostan <igorkostan@gmail.com>
2024-05-19 22:15:15 -0700

    wokwi

---
+++ lessons/wokwi/main.py
@@ -0,0 +1,10 @@
+import time
+from machine import Pin
+
+time.sleep(0.25)  # Wait for USB to become ready
+led = Pin(5, Pin.OUT)
+
+while True:
+  led.toggle()
+  time.sleep(0.5)
+  print("Hello, Pi Pico!")
