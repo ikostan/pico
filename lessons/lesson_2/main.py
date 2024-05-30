401e3ac55679e44faeeb4369a199c6fd2f14749c
Egor Kostan <igorkostan@gmail.com>
2024-05-29 19:53:38 -0700

    WOKWI for lesson 2

--- lessons/lesson_2/main.py
+++ lessons/lesson_2/main.py
@@ -3,6 +3,7 @@
 Raspberry Pi Pico W LESSON 2: Understanding and Using Breadboards
 https://www.youtube.com/watch?v=eGdrtikKc5U&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyAhC5&index=2
 """
+
 import time
 from machine import Pin
 
@@ -12,7 +13,9 @@ led = Pin(15, Pin.OUT)
 if __name__ == '__main__':
 
     while True:
+        print("LED is off")
         led.value(0)
         time.sleep(0.5)
+        print("LED is on")
         led.value(1)
         time.sleep(1)
