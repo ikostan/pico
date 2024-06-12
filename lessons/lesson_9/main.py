9f4999f84a82e1f51cb44910949be5e908444b97
Egor Kostan <igorkostan@gmail.com>
2024-06-11 19:58:14 -0700

    Fixes for lesson 9

--- lessons/lesson_9/main.py
+++ lessons/lesson_9/main.py
@@ -2,12 +2,18 @@
 Raspberry Pi Pico W LESSON 9: Getting Analog Output Using PWM (Pulse Width Modulation)
 https://www.youtube.com/watch?v=GXA1Y6lA14A&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyAhC5&index=12
 """
-
-from machine import (PWM, Pin)
 from time import sleep
+from machine import (PWM, Pin)
 
-pin_num = 16
-analogOut = PWM(Pin(pin_num))
+
+class VoltageError(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+PIN_NUM = 16
+analogOut = PWM(Pin(PIN_NUM))
 analogOut.freq(1000)
 analogOut.duty_u16(0)
 
@@ -20,16 +26,16 @@ while True:
             print("Exiting program now...")
             analogOut.duty_u16(0)
             break
-        else:
-            user_input = float(user_input)
 
-            if user_input < 0 or user_input > 3.3:
-                raise Exception(f"Invalid voltage value -> {user_input}.")
+        user_input = float(user_input)
+
+        if user_input < 0 or user_input > 3.3:
+            raise VoltageError(f"Invalid voltage value -> {user_input}.")
 
         pwm_val = int((65550 / 3.3) * user_input)
         analogOut.duty_u16(pwm_val)
 
-    except Exception as e:
+    except VoltageError as e:
         print(e)
 
     finally:
