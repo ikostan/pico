87ae81e875f0ea96009f3d9cd1748ffef50086cb
Egor Kostan <igorkostan@gmail.com>
2024-06-18 19:34:31 -0700

    Update main.py
    linting fixes

--- lessons/lesson_12/main.py
+++ lessons/lesson_12/main.py
@@ -7,14 +7,14 @@ from time import sleep
 from machine import (Pin, PWM)
 
 
-colors = {'red': (255,0,0),
-          'green': (0,128,0),
-          'blue': (0,0,255),
-          'cyan': (0,255,255),
-          'magenta': (255,0,255),
-          'yellow': (255,255,0),
-          'orange': (255,165,0),
-          'white': (255,255,255)}
+colors = {'red': (255, 0, 0),
+          'green': (0, 128, 0),
+          'blue': (0, 0, 255),
+          'cyan': (0, 255, 255),
+          'magenta': (255, 0, 255),
+          'yellow': (255, 255, 0),
+          'orange': (255, 165, 0),
+          'white': (255, 255, 255)}
 
 pins = {'RED': 13,
         'GREEN': 14,
