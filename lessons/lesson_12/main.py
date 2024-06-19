6dd7258e8e003ccd24744d379f4b78394aee888e
Egor Kostan <igorkostan@gmail.com>
2024-06-18 17:31:13 -0700

    Update main.py

--- lessons/lesson_12/main.py
+++ lessons/lesson_12/main.py
@@ -16,36 +16,41 @@ pwms = [PWM(Pin(pins[RED])),
         PWM(Pin(pins[GREEN])),
         PWM(Pin(pins[BLUE]))]
 
+
 def pwms_off():
     # noinspection PyTypeChecker
     for pwm in pwms:
         pwm.duty_u16(0)
+
 
 def initial_setup(val=1000):
     # noinspection PyTypeChecker
     for pwm in pwms:
         pwm.freq(val)
 
+
 def red_on(val=65550):
-   pwms_off()
-   pwms[RED].duty_u16(val)
-   print('Red')
+    pwms_off()
+    pwms[RED].duty_u16(val)
+    print('Red')
+
 
 def green_on(val=65550):
-   pwms_off()
-   pwms[GREEN].duty_u16(val)
-   print('Green')
+    pwms_off()
+    pwms[GREEN].duty_u16(val)
+    print('Green')
+
 
 def blue_on(val=65550):
-   pwms_off()
-   pwms[BLUE].duty_u16(val)
-   print('Blue')
+    pwms_off()
+    pwms[BLUE].duty_u16(val)
+    print('Blue')
 
 
 if __name__ == '__main__':
 
     initial_setup()
-    
+
     while True:
         red_on()
         sleep(0.5)
