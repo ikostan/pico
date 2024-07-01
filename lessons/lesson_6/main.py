9d56b1252c7dffb60404b0c73eb84fb06e8e2fb0
Egor Kostan <igorkostan@gmail.com>
2024-06-30 21:44:18 -0700

    Update main.py
    Showing 2 of 2 total issues
    Constant name cmd_input doesn't conform to UPPER_CASE naming style OPEN
            cmd_input = input("What would you like LED to be (On/Off/Toggle)? ")
    Severity: InfoFound in lessons/lesson_6/main.py by pylint
    Constant name led doesn't conform to UPPER_CASE naming style OPEN
    led = Pin(PIN_N, Pin.OUT)
    Severity: InfoFound in lessons/lesson_6/main.py by pylint

--- lessons/lesson_6/main.py
+++ lessons/lesson_6/main.py
@@ -6,20 +6,20 @@ https://www.youtube.com/watch?v=mS4YcJ0FcOU&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyA
 from machine import Pin  # pylint: disable=import-error
 
 PIN_N = 14
-led = Pin(PIN_N, Pin.OUT)
+LED = Pin(PIN_N, Pin.OUT)
 
 
 if __name__ == '__main__':
-    led.value(0)
+    LED.value(0)
 
     while True:
-        cmd_input = input("What would you like LED to be (On/Off/Toggle)? ")
-        print(cmd_input)
-        if cmd_input.lower() == 'on':
-            led.value(1)
-        elif cmd_input.lower() == 'off':
-            led.value(0)
-        elif cmd_input.lower() == 'toggle':
-            led.toggle()
+        CMD_INPUT = input("What would you like LED to be (On/Off/Toggle)? ")
+        print(CMD_INPUT)
+        if CMD_INPUT.lower() == 'on':
+            LED.value(1)
+        elif CMD_INPUT.lower() == 'off':
+            LED.value(0)
+        elif CMD_INPUT.lower() == 'toggle':
+            LED.toggle()
         else:
-            print(f"{cmd_input} is unspecified...")
+            print(f"{CMD_INPUT} is unspecified...")
