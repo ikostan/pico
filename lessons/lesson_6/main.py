955207b8203188f3351f41a3c8b1918f05c329d8
Egor Kostan <igorkostan@gmail.com>
2024-07-01 15:42:46 -0700

    Update main.py

--- lessons/lesson_6/main.py
+++ lessons/lesson_6/main.py
@@ -5,15 +5,15 @@ https://www.youtube.com/watch?v=mS4YcJ0FcOU&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyA
 
 from machine import Pin  # pylint: disable=import-error
 
-PIN_N = 14
-LED = Pin(PIN_N, Pin.OUT)
+PIN_N: int = 14
+LED: Pin = Pin(PIN_N, Pin.OUT)
 
 
 if __name__ == '__main__':
     LED.value(0)
 
     while True:
-        CMD_INPUT = input("What would you like LED to be (On/Off/Toggle)? ")
+        CMD_INPUT: str = input("What would you like LED to be (On/Off/Toggle)? ")
         print(CMD_INPUT)
         if CMD_INPUT.lower() == 'on':
             LED.value(1)
