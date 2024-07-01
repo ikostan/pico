ba963aff1a299e6ed14e0429fd1e5cc1ce8ccfe8
Egor Kostan <igorkostan@gmail.com>
2024-06-30 19:19:53 -0700

    Update main.py

--- lessons/lesson_1/main.py
+++ lessons/lesson_1/main.py
@@ -1,24 +1,37 @@
+
 """
 Raspberry Pi Pico W LESSON 1: Write Your First Program for Absolute Beginners
 https://www.youtube.com/watch?v=SL4_oU9t8Ss&list=PLGs0VKk2DiYz8js1SJog21cDhkBqyAhC5
 """
 
 import time
-import random
-from machine import Pin
+from machine import Pin  # pylint: disable=import-error
 
 
-random.randrange(0, 1)
-led = Pin(6, Pin.OUT)
+LED = Pin(6, Pin.OUT)
+# fmt: off
+# Black will not reformat lines that contain fmt
+IS_ON = 0
+# fmt: on
+
+
+def toggle_is_on(val: int) -> int:
+    """
+    Update IS_ON value
+    """
+    return 1 if val == 0 else 0
+
+
+def toggle_led(val: int) -> None:
+
+    """
+    Toggle LED
+    """
+    LED.value(val)
+
 
 if __name__ == '__main__':
     while True:
-        IS_ON = 0
-        led.value(IS_ON)
-        t = random.random()
-        t = round(t, 2)
-        print(f"sleep: {t}, LED: {IS_ON}.")
-        time.sleep(t)
-        IS_ON = 1
-        led.value(IS_ON)
-        print(f"sleep: {t}, LED: {IS_ON}.")
+        toggle_led(IS_ON)
+        time.sleep(0.3)
+        IS_ON = toggle_is_on(IS_ON)  # fmt: skip
