1de85605d7bf63f7e2db2e1235484b7b84ca6bf5
Egor Kostan <igorkostan@gmail.com>
2024-07-04 22:06:40 -0700

    Update lesson_9_and_10_test.py

--- tests/lesson_9_and_10_test.py
+++ tests/lesson_9_and_10_test.py
@@ -3,6 +3,7 @@ Test suite for lesson 9 and 10.
 """
 
 import unittest
+from unittest.mock import MagicMock
 # pylint: disable=import-error
 from lessons.lesson_9_and_10.main import (
     VoltageError,
@@ -16,6 +17,28 @@ class Lesson9And10TestCase(unittest.TestCase):
     Test case for lesson 9 and 10.
     """
 
+    def test_analog_out_freq(self):
+        """
+        Mocking and testing analogOut.freq()
+        :return:
+        """
+        f = 1000
+        analogOut.freq = MagicMock()
+        analogOut.freq(f)
+        analogOut.freq.return_value = f
+        self.assertEqual(analogOut.freq(), f)
+
+    def test_analog_out_duty_u16(self):
+        """
+        Mocking and testing analogOut.duty_u16()
+        :return:
+        """
+        v = 0
+        analogOut.freq = MagicMock()
+        analogOut.duty_u16(v)
+        analogOut.duty_u16.return_value = v
+        self.assertEqual(analogOut.duty_u16(), v)
+
     def test_pin_num_data_type(self):
         """
         PIN_NUM data type should be int.
@@ -28,7 +51,7 @@ class Lesson9And10TestCase(unittest.TestCase):
         PIN_NUM value should be 16.
         :return:
         """
-        self.assertIsEqual(PIN_NUM, 16)
+        self.assertEqual(PIN_NUM, 16)
 
     def test_voltage_error(self):
         """
