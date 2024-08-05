dfd66e536010e3a2fa64deffb5754d2f918b7082
Egor Kostan <igorkostan@gmail.com>
2024-08-04 21:01:01 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,10 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
