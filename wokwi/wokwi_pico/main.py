e33e32ff0d7eb76cccc89da7357500f2b268f653
Egor Kostan <igorkostan@gmail.com>
2024-08-05 18:52:51 -0700

    Update main.py

--- wokwi/wokwi_pico/main.py
+++ wokwi/wokwi_pico/main.py
@@ -8,7 +8,8 @@ from machine import Pin
 sleep(0.25)
 led = Pin(5, Pin.OUT)
 
-while True:
-    led.toggle()
-    sleep(0.5)
-    print("Hello, Pi Pico!")
+if __name__ == '__main__':
+    while True:
+        led.toggle()
+        sleep(0.5)
+        print("Hello, Pi Pico!")
