1eec4c51993ef1045ae2c0c9f316a1e592ec92f1
Egor Kostan <igorkostan@gmail.com>
2024-08-05 18:52:55 -0700

    Update main.py

--- wokwi/wokwi_pico_w/main.py
+++ wokwi/wokwi_pico_w/main.py
@@ -8,7 +8,8 @@ from machine import Pin
 time.sleep(0.25)  # Wait for USB to become ready
 led = Pin(5, Pin.OUT)
 
-while True:
-    led.toggle()
-    time.sleep(0.5)
-    print("Hello, Pi Pico!")
+if __name__ == '__main__':
+    while True:
+        led.toggle()
+        time.sleep(0.5)
+        print("Hello, Pi Pico!")
